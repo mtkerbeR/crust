6692c8bee8b0c8ac7083d3fbe1deca424a2f4a94
Michael Kerber <mtkerber@gmail.com>
2022-06-02 20:03:59 +0200

    initial commit

---
+++ src/lib.rs
@@ -0,0 +1,30 @@
+use pyo3::prelude::*;
+
+/// Returns the sum of two numbers (only Rust)
+fn rust_sum(a: i64, b: i64) -> i64 {
+    a + b
+}
+
+/// Returns the sum of two numbers, wrapped by pyo3
+#[pyfunction]
+fn pyo3_sum(a: usize, b: usize) -> PyResult<usize> {
+    Ok(a + b)
+}
+
+/// A Python module implemented in Rust.
+#[pymodule]
+fn foobar(_py: Python, m: &PyModule) -> PyResult<()> {
+    m.add_function(wrap_pyfunction!(pyo3_sum, m)?)?;
+    Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_rust_sum() {
+        assert_eq!(rust_sum(2, 1), 3);
+        assert_eq!(rust_sum(1, 2), 3);
+    }
+}
